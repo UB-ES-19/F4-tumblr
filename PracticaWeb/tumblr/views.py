466a19c068050bd193f4595c2c1e3a6167c51f9c
AleAlfonsoHdz <alfonsoalejandro023@gmail.com>
2019-10-04 01:56:48 +0200

    Added tumblr login backend - check email first. then login as usual.

--- PracticaWeb/tumblr/views.py
+++ PracticaWeb/tumblr/views.py
@@ -1,25 +1,42 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from tumblr.forms import SignUpForm
+
 
 def index(request):
     return render(request, 'templates/index.html')
 
 
-
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
             return HttpResponseRedirect(reverse("index"))
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, "registration/register.html", {
         'form': form,
     })
+
+
+def check_email(request):
+    if request.method == 'POST':
+        search_email = request.POST.get('email', None)
+        try:
+            User.objects.get(email=search_email)
+            return HttpResponseRedirect(reverse("login"))
+        except User.DoesNotExist:
+            return HttpResponse("This email doesn't have a Tumblr account.")
+    else:
+        return render(request, "registration/login-email.html")
