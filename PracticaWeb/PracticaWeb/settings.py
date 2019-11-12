bdc04e37c05f9bbbf0fe313a7c5403db31883de0
TTinan <martirubioestrada@gmail.com>
2019-11-12 16:42:33 +0100

    Migration to MySQL done

--- PracticaWeb/PracticaWeb/settings.py
+++ PracticaWeb/PracticaWeb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,14 +80,23 @@ WSGI_APPLICATION = 'PracticaWeb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dumblr',
+        'USER': 'root',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
