74c5dc1b2858ac01e8d29c9dff259968ea3227fd
TTinan <martirubioestrada@gmail.com>
2019-11-16 19:08:07 +0100

    Better data model

--- PracticaWeb/tumblr/models.py
+++ PracticaWeb/tumblr/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Audio(models.Model):
     user=models.CharField(max_length=500)
     audiofile= models.FileField(upload_to='audios/', null=True, verbose_name="")
-
+    type = models.CharField(max_length=50, default='audio', editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.audiofile)
 
@@ -13,6 +14,8 @@ class Chat(models.Model):
     # Template of video file, to change
     name=models.CharField(max_length=500)
     user=models.CharField(max_length=500)
+    type = models.CharField(max_length=50, default='chat', editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
     videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
 
     def __str__(self):
@@ -20,6 +23,8 @@ class Chat(models.Model):
 
 class Image(models.Model):
     user=models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=50, default='image', editable=False)
     imagefile= models.FileField(upload_to='images/', null=True, verbose_name="")
 
     def __str__(self):
@@ -29,6 +34,8 @@ class Link(models.Model):
     # Template of video file, to change
     name= models.CharField(max_length=500)
     user=models.CharField(max_length=500)
+    type = models.CharField(max_length=50, default='link', editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
     videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
 
     def __str__(self):
@@ -36,6 +43,8 @@ class Link(models.Model):
 
 class Quote(models.Model):
     id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=50, default='quote', editable=False)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
     quote = models.TextField(max_length=200, default='')
     source = models.TextField(max_length=200, default='')
@@ -44,6 +53,8 @@ class Text(models.Model):
     # Template of video file, to change
     name= models.CharField(max_length=500)
     user=models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=50, default='text', editable=False)
     videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
 
     def __str__(self):
@@ -52,6 +63,8 @@ class Text(models.Model):
 class Video(models.Model):
     # Template of video file, to change
     user=models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=50, default='video', editable=False)
     videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
 
     def __str__(self):
