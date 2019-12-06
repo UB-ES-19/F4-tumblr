083e892e9d9720fba0abb8da88a48181b71f64dd
Xavier Ripoll <xaviripo97@gmail.com>
2019-12-06 11:37:33 +0100

    #47: Ask only once for email on login

--- PracticaWeb/tumblr/views.py
+++ PracticaWeb/tumblr/views.py
@@ -184,8 +184,8 @@ def check_email(request):
     if request.method == 'POST':
         search_email = request.POST.get('email', None)
         try:
-            User.objects.get(email=search_email)
-            return HttpResponseRedirect(reverse("login"))
+            user = User.objects.get(email=search_email)
+            return HttpResponseRedirect(reverse("login") + "?username=" + user.username + "&email=" + search_email)
         except User.DoesNotExist:
             return HttpResponse("This email doesn't have a Dumblr account.")
     else:
